d6f81a857352395035b3d46737b6b6e01fddf890
Vohizy <hei.vohizy@gmail.com>
2022-12-19 06:09:12 +0300

    feat: create repository of Player

--- src/main/java/com/example/springsecuritydemo/repository/PlayerRepository.java
+++ src/main/java/com/example/springsecuritydemo/repository/PlayerRepository.java
@@ -1,2 +1,9 @@
-package com.example.springsecuritydemo.repository;public interface PlayerRepository {
+package com.example.springsecuritydemo.repository;
+
+import com.example.springsecuritydemo.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player ,Integer> {
 }
