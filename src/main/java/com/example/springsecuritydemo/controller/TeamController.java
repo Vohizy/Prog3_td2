62f45acaf9767469bd6a88f8a4c1d93cef1519fc
Vohizy <hei.vohizy@gmail.com>
2022-12-19 07:58:47 +0300

    feat: add team controller

--- src/main/java/com/example/springsecuritydemo/controller/TeamController.java
+++ src/main/java/com/example/springsecuritydemo/controller/TeamController.java
@@ -1,4 +1,21 @@
 package com.example.springsecuritydemo.controller;
 
+import com.example.springsecuritydemo.model.Team;
+import com.example.springsecuritydemo.service.TeamService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class TeamController {
+    private final TeamService service;
+
+    @GetMapping("/team")
+    public List<Team> getAll(){
+        return service.getAll();
+    }
+
 }
